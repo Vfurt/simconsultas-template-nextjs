89921b09b9440b2bfcd329c4695dd36a2a29298d
Vinicius Furtado <vinicius.furtado@simconsultas.com.br>
2020-09-23 20:51:50 -0300

    InitialCommit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import React from 'react'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>HomePage</title>
+      </Head>
+
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
